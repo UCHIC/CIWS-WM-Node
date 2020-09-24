ee155c505fb5c859f6b99da6cde8764193207af4
abeck <52251275+arlebeckwith@users.noreply.github.com>
2020-09-24 11:37:40 -0600

    Update processData.py

--- Software/processData.py
+++ Software/processData.py
@@ -17,10 +17,9 @@ def processData(toSend, toStore):
 	for files in types:
 		files_grabbed.extend(glob.glob(files))
 	for filenames in files_grabbed:
-
-		if toSend is not '1' or toStore is not '1':
+		if toSend in ['2','3'] or toStore is not '1':
 			for disagcsv in crunchData(filenames):
-				if toSend is not '1':
+				if toSend in ['2','3']:
 					with open(disagcsv, 'rb') as data_file:
 						files = [('data_file[]', data_file), ]
 						upload_token = requests.post(upload_token_url,data={'token': client_passcode, 'filenames': disagcsv})
@@ -31,14 +30,12 @@ def processData(toSend, toStore):
 					shutil.move(cd + disagcsv, out + disagcsv)
 				else:
 					os.remove(cd + disagcsv)
-
-		if toSend is not '2':
+		if toSend in ['1','3']:
 			with open(filenames, 'rb') as data_file:
 				files = [('data_file[]', data_file), ]
 				upload_token = requests.post(upload_token_url, data={'token': client_passcode, 'filenames': filenames})
 				upload_response = requests.post(upload_url, headers={'Authorization': f'Bearer {upload_token.text}'},files=files)
 				print(upload_response.text)
-
 		if toStore is not '2':
 			shutil.move(cd + filenames, out + filenames)
 		else:

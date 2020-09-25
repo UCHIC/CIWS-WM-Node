5c01810f0869254712c69238913cdb0d6a1aad11
abeck <52251275+arlebeckwith@users.noreply.github.com>
2020-09-25 09:45:52 -0600

    Update processData.py

--- Software/processData.py
+++ Software/processData.py
@@ -12,31 +12,35 @@ def processData(toSend, toStore):
 	out = '/home/pi/Software/savedData/'
 	if not os.path.exists(out):
 		os.makedirs(out)
-	types = ('*.CSV', '*.csv')
 	files_grabbed = []
-	for files in types:
+	for files in ('*.CSV', '*.csv'):
 		files_grabbed.extend(glob.glob(files))
 	for filenames in files_grabbed:
 		if toSend in ['2','3'] or toStore is not '1':
-			for disagcsv in crunchData(filenames):
+			try:
+				disaglist = crunchData(filenames)
+			except:
+				print('Failed to crunch data')
+			for disagcsv in disaglist:
 				if toSend in ['2','3']:
-					with open(disagcsv, 'rb') as data_file:
-						files = [('data_file[]', data_file), ]
-						upload_token = requests.post(upload_token_url,data={'token': client_passcode, 'filenames': disagcsv})
-						upload_response = requests.post(upload_url,headers={'Authorization': f'Bearer {upload_token.text}'},files=files)
-						print(upload_response.text)
-
+					send(disagcsv)
 				if toStore is not '1':
 					shutil.move(cd + disagcsv, out + disagcsv)
 				else:
 					os.remove(cd + disagcsv)
 		if toSend in ['1','3']:
-			with open(filenames, 'rb') as data_file:
-				files = [('data_file[]', data_file), ]
-				upload_token = requests.post(upload_token_url, data={'token': client_passcode, 'filenames': filenames})
-				upload_response = requests.post(upload_url, headers={'Authorization': f'Bearer {upload_token.text}'},files=files)
-				print(upload_response.text)
+			send(filenames)
 		if toStore is not '2':
 			shutil.move(cd + filenames, out + filenames)
 		else:
 			os.remove(cd + filenames)
+			
+def send(file):
+	try:
+		with open(file, 'rb') as data_file:
+			files = [('data_file[]', data_file), ]
+			upload_token = requests.post(upload_token_url, data={'token': client_passcode, 'filenames': file})
+			upload_response = requests.post(upload_url, headers={'Authorization': f'Bearer {upload_token.text}'},files=files)
+			print(upload_response.text)
+	except:
+		print('Failed to send', file)

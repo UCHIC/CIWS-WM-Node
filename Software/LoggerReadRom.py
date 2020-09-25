dcad7f4b8703ff975842e5df12d4a0bbcf6adc6d
abeck <52251275+arlebeckwith@users.noreply.github.com>
2020-09-25 15:18:06 -0600

    Update LoggerReadRom.py

--- Software/LoggerReadRom.py
+++ Software/LoggerReadRom.py
@@ -10,7 +10,7 @@ dataTuple = Logger.loadData()   # Read the data from the EEPROM chip
 Logger.setRomFree()             # Tell the AVR datalogger that the EEPROM chip is no longer in use.
 
 # Write data to file
-filename= "/home/pi/Software3/data/site" + Logger.getSiteNumber().zfill(4) + "_20" + '-'.join([str(dataTuple[1]).zfill(2), str(dataTuple[2]).zfill(2), str(dataTuple[3]).zfill(2)])\
+filename= "/home/pi/Software/data/site" + Logger.getSiteNumber().zfill(4) + "_20" + '-'.join([str(dataTuple[1]).zfill(2), str(dataTuple[2]).zfill(2), str(dataTuple[3]).zfill(2)])\
 	+ 'T' + str(dataTuple[4]).zfill(2) + str(dataTuple[5]).zfill(2) + str(dataTuple[6]).zfill(2) + ".csv"
 
 try:

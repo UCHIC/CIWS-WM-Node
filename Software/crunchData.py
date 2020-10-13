d8024dfc54f2d415f2a25b5afd14320d9609f7e2
abeck <52251275+arlebeckwith@users.noreply.github.com>
2020-10-13 15:44:42 -0600

    Update crunchData.py

--- Software/crunchData.py
+++ Software/crunchData.py
@@ -1,6 +1,5 @@
-import WEUDseven
 def crunchData(file):  # pass in file name to be processed
 	try:
-		return [WEUDseven.WEUD(file)]           #Return list of file names for output data
+		return []           #Return list of file names for output data
 	except:
 		return []
